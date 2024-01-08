0717f0ffb02d725b508ae0c353ff624d2ee9898a
Katherine Hallissy Ayala <34048354+KatherineHallissyAyala@users.noreply.github.com>
2024-01-08 12:52:53 -0500

    playing with methods

--- pet.java
+++ pet.java
@@ -1,3 +1,9 @@
-public class Dog = {
-    System.out.println("Woof");
+package tutorial1;
+
+public class Main{
+        
+        public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
 }
